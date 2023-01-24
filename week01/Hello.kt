bbeddad401cee231f4edc93d595faa5eb24d1f8d
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-01-24 09:49:36 -0600

    Create Hello.kt

---
+++ week01/Hello.kt
@@ -0,0 +1,10 @@
+/**
+* My first Kotlin File / Jonathan Ventura
+*/
+
+fun main() {
+    println("Hello, world!!!")
+    println("i am learning code")
+    println("Mi nombre es Jonathan")
+    }
+    
