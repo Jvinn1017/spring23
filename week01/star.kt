0906bcc1a651e31f84492b73ead4fd49e0761926
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-02-12 21:34:16 -0600

    Create star.kt

---
+++ week01/star.kt
@@ -0,0 +1,12 @@
+/**Jonathan Ventura 
+  2/7/23*/
+ 
+fun main() {
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
