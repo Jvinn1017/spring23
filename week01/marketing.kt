3c969e2f95343ae8b8762276b44e379ef055077d
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-02-12 21:06:16 -0600

    Create marketing.kt

---
+++ week01/marketing.kt
@@ -0,0 +1,16 @@
+/**Jonathan Ventura 
+  2/7/23*/
+ 
+fun main() 
+{
+   var friendsCount = 500
+    
+   if (friendsCount >= 500)
+    {
+        println("Adopt a Dog Today!!")
+    }
+    else
+        {
+            println("Buy Cat Food At 20% Off!!!")
+        }
+}
