54499d8b770c8d532b24307fd6bec11a22a82339
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-02-12 20:52:10 -0600

    Create Variables.kt

---
+++ week01/Variables.kt
@@ -0,0 +1,18 @@
+/**Jonathan Ventura 
+  2/7/23*/
+ 
+
+fun main() {
+   var MyAge = 26
+   var averageAge = 30
+    println(MyAge)
+    println(averageAge)
+    
+    var testNumber = 6
+    var evenOdd = testNumber //evenOdd changes when testNumber changes
+    println(evenOdd)
+    
+    var answer= 0
+    answer = answer+1+10*10+3
+    print(answer)
+}
