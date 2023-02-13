e40e5ef3e3ab6d172eafb63e898e997f644a6952
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-02-12 21:30:35 -0600

    Create salary.kt

---
+++ week01/salary.kt
@@ -0,0 +1,10 @@
+/**Jonathan Ventura 
+  2/7/23*/
+ 
+fun main() {
+   val name = "Jonathan";
+   val age = 26;
+   val annualPay = 100000;
+   println("My name is $name, my age is $age and I hope to earn $annualPay per year")
+   
+}
