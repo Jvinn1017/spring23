b77f793718064e77a90d97b6474fe2a25922720f
Jvinn1017 <123103528+Jvinn1017@users.noreply.github.com>
2023-02-16 09:02:56 -0600

    Create stringMani.kt

---
+++ week01/stringMani.kt
@@ -0,0 +1,8 @@
+fun main()  {  
+    println("Enter your favorite city name:")
+    val cityName = "Dallas"
+    println("The number of characters in the city name is ${cityName.length}")
+    println("The name of the city in uppercase letters is ${cityName.toUpperCase()}")
+    println("The name of the city in lowercase letters is ${cityName.toLowerCase()}")
+    println("The first character in the name of the city is ${cityName[0]}")
+}
